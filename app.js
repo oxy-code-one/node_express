49ce862384e8d72151e83b194cfdf1d757b06f68
oxy-code-one <112403121+oxy-code-one@users.noreply.github.com>
2022-10-18 11:14:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
 const app = express();
-
+let listenport = process.env.PORT || 80;
 app.get("/",(req,res)=>{
     res.end("hello from shubham.");
 });
 
-app.listen(5000,()=>{
+app.listen(listenport,()=>{
     console.log("server started ");
 });
